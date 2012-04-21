9917de5c42f8f16757731cf52aef936001504a01
Krzysztof "Bushee" Nowaczyk <bushee01@gmail.com>
2012-04-21 14:48:24 +0200

    modified test case to test empty production rules

--- test.php
+++ test.php
@@ -12,6 +12,7 @@
  */
 require_once 'slr.php';
 
+// TODO pretty classes for parser config, ruleset and rules
 $parserConfig = array(
 	'start' => 'E',
 	'rules' => array(
@@ -29,25 +30,35 @@ $parserConfig = array(
 				create_function('$v', 'return $v[1];')
 			),
 			array(
-				array('id'),
+				array('NUM'),
 				create_function('$v', 'return $v[0];')
 			)
 		),
+	    'NUM' => array(
+	        array(
+	            array('digit', 'NUM'),
+	            create_function('$v', 'return $v[0] . $v[1];')
+	        ),
+	        array(
+	            array(), // TODO proper handling of empty rules
+            )
+        )
 	),
 	'success' => create_function('$v', 'echo "$v\n"; return $v;')
 );
 
+// TODO pretty classes for lexer config and rules
 $lexerConfig = array(
 	'initial' => array(
 		array('string', '+', create_function('&$value', 'return \'+\';')),
 		array('string', '*', create_function('&$value', 'return \'x\';')),
 		array('string', '(', create_function('&$value', 'return \'(\';')),
 		array('string', ')', create_function('&$value', 'return \')\';')),
-		array('regex', '/[0-9]+/', create_function('&$value', 'return \'id\';'))
+		array('regex', '/[0-9]/', create_function('&$value', 'return \'digit\';'))
 	)
 );
 
-$string = '3*(5+2)';
+$string = '3*(5+2)+13';
 
 $lexer = new SLR_Lexer($lexerConfig);
 $slr = new SLR_SLR($parserConfig);
